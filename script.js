fde9f7648f974b39ecbf1e6ccf0e437a716f9a84
Colinjpjp <136663156+Colinjpjp@users.noreply.github.com>
2023-08-05 16:06:16 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let pro=prompt("USER??") ;
+const user="jose";
+const passUser="123";
+let pro2=prompt("password?");
+const user2="Ada";
+
+if(user===pro){
+    if(passUser===pro2 && user===pro){
+        alert("is correct your password")
+    }
+    else{
+    alert("password incorrect")
+    }
+}else{
+    alert("Try Again")
+} 
+
